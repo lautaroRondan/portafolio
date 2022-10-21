fa413b89ca5cb3fb65b567bb09a477c0d3ed4e15
lautaro <lautarorondan96@gmail.com>
2022-10-21 00:22:35 -0300

    update proyects

--- src/components/Proyects.jsx
+++ src/components/Proyects.jsx
@@ -1,70 +1,33 @@
 import React from 'react'
 import desarrollador from '../assets/programador.png'
+import { trabajos } from '../data/proyectos'
+
 
 const Proyects = () => {
   return (
     <div id='proyects' className='proyects'>
 
       <h1>Mis Proyectos</h1>
+
+
       <section className="content">
-        <article className="proyect-item">
-          <img src={desarrollador} />
-          <h2 className="title"> Desarrollo web</h2>
-          <p className="description">Master JavaScript</p>
 
-          <button className="btnProyect"> Ver proyecto</button>
-          <button className="btnRepository"> Ver repositorio</button>
-        </article>
+        {
+          trabajos.map(trabajos => {
+            return (
+              <article className="proyect-item" key={trabajos.id}>
+                <img src={desarrollador} />
+                <h2 className="title"> {trabajos.nombre}</h2>
+                <p className="description">{trabajos.categorias}</p>
+                <p className="description">{"herramientas: " + trabajos.tecnologias}</p>
+                <button className="btnProyect"> Ver proyecto</button>
+                <button className="btnRepository"> Ver repositorio</button>
+              </article>
+            )
+          })
+        }
 
-        <article className="proyect-item">
-          <img src={desarrollador} />
-          <h2 className="title"> Desarrollo web</h2>
-          <p className="description">Master JavaScript</p>
-
-          <button className="btnProyect"> Ver proyecto</button>
-          <button className="btnRepository"> Ver repositorio</button>
-        </article>
-
-        <article className="proyect-item">
-          <img src={desarrollador} />
-          <h2 className="title"> Desarrollo web</h2>
-          <p className="description">Master JavaScript</p>
-
-          <button className="btnProyect"> Ver proyecto</button>
-          <button className="btnRepository"> Ver repositorio</button>
-        </article>
-
-        <article className="proyect-item">
-          <img src={desarrollador} />
-          <h2 className="title"> Desarrollo web</h2>
-          <p className="description">Master JavaScript</p>
-
-          <button className="btnProyect"> Ver proyecto</button>
-          <button className="btnRepository"> Ver repositorio</button>
-        </article>
       </section>
-
-
-
-
-
-
-      {/* {
-            trabajos.map(trabajos=>{
-              return(
-                <article key={trabajos.id}>
-                  <div classNameName='mask'> 
-                      <img src='\images\facebook.jpg' alt="sa"></img>
-                  </div>
-                  <span>{trabajos.categorias}</span>
-                  <h2><Link to={trabajos.url}>{trabajos.nombre}</Link></h2>
-                  <h3>{trabajos.tecnologias}</h3>
-                  </article>
-              )
-            })
-
-          } */}
-
 
     </div>
   )
--- src/data/proyectos.js
+++ src/data/proyectos.js
@@ -4,21 +4,32 @@ export const trabajos = [
         'nombre': 'victor roblrs web',
         'url': 'victorroblesweb.es',
         'tecnologias': 'HTML, CSS, JS, PHP, MySQL',
-        'categorias': 'Desarrollo web'
+        'categorias': 'Desarrollo web',
+        'imagen': 'desarrollador'
     },
     {
         'id': 'facebook',
         'nombre': 'Facebook',
         'url': 'facebook.com',
         'tecnologias': 'HTML, CSS, JS, PHP, MySQL',
-        'categorias': 'Desarrollo web'
+        'categorias': 'Desarrollo web',
+        'imagen': 'desarrollador'
     },
     {
         'id': 'instagram',
         'nombre': 'instagram',
         'url': 'instagram.com',
         'tecnologias': 'HTML, CSS, JS, React, mongo',
-        'categorias': 'Desarrollo web'
+        'categorias': 'Desarrollo web',
+        'imagen': 'desarrollador'
+    },
+    {
+        'id': 'instagram1',
+        'nombre': 'instagram',
+        'url': 'instagram.com',
+        'tecnologias': 'HTML, CSS, JS, React, mongo',
+        'categorias': 'Desarrollo web',
+        'imagen': 'desarrollador'
     }
 
 ]
